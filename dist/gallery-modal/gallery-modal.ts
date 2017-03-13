7bcd01b47017bba9d6f48d836bb511b2ceb234e3
rlindgren <ryan.lindgren@gmail.com>
2017-03-13 18:52:44 -0400

    add dist to repo

---
+++ dist/gallery-modal/gallery-modal.ts
@@ -0,0 +1,86 @@
+import { Component, ViewChild, ViewEncapsulation, ElementRef } from '@angular/core';
+import { ViewController, NavParams, Slides, Content, Platform } from 'ionic-angular';
+import { Photo } from '../interfaces/photo-interface';
+import { Subject }    from 'rxjs/Subject';
+
+@Component({
+  encapsulation: ViewEncapsulation.None,
+  selector: 'gallery-modal',
+  templateUrl: 'gallery-modal.html',
+})
+export class GalleryModal {
+  @ViewChild('slider') slider: Slides;
+  @ViewChild('content') content: Content;
+
+  public photos: Photo[];
+  private sliderDisabled: boolean = false;
+  private initialSlide: number = 0;
+  private currentSlide: number = 0;
+  private sliderLoaded: boolean = false;
+  private closeIcon: string = 'arrow-back';
+  private parentSubject: Subject<any> = new Subject();
+
+  constructor(private viewCtrl: ViewController, params: NavParams, private element: ElementRef, private platform: Platform) {
+    this.photos = params.get('photos') || [];
+    this.closeIcon = params.get('closeIcon') || 'arrow-back';
+    this.initialSlide = params.get('initialSlide') || 0;
+  }
+
+  /**
+   * Closes the modal (when user click on CLOSE)
+   */
+  public dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+  private resize(event) {
+    this.slider.update();
+
+    let width = this.element['nativeElement'].offsetWidth;
+    let height = this.element['nativeElement'].offsetHeight;
+
+    this.parentSubject.next({
+      width: width,
+      height: height,
+    });
+  }
+
+  private orientationChange(event) {
+    // TODO: See if you can remove timeout
+    window.setTimeout(() => {
+      this.resize(event);
+    }, 150);
+  }
+
+  /**
+   * When the modal has entered into view
+   */
+  private ionViewDidEnter() {
+    this.resize(false);
+    this.sliderLoaded = true;
+  }
+
+  /**
+   * Disables the scroll through the slider
+   *
+   * @param  {Event} event
+   */
+  private disableScroll(event) {
+    if (!this.sliderDisabled) {
+      this.currentSlide = this.slider.getActiveIndex();
+      this.sliderDisabled = true;
+    }
+  }
+
+  /**
+   * Enables the scroll through the slider
+   *
+   * @param  {Event} event
+   */
+  private enableScroll(event) {
+    if (this.sliderDisabled) {
+      this.slider.slideTo(this.currentSlide, 0, false);
+      this.sliderDisabled = false;
+    }
+  }
+}
